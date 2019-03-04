628f6d18e5761cb12847563e89e43fd282058d78
Jennifer Hsiao <jchmai21@gmail.com>
2019-03-04 05:28:08 -0800

    deleted commented code

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function getTeleportLocationData(objArr) {
         .then(obj => {
             console.log(`Ip address data is`, obj);
             return getDataByIpInput(obj)
-            /*return getTeleportScores(obj);*/
+            
 
         })
 }
@@ -193,72 +193,12 @@ function updateDOM() {
         displaySecondaryCitySummary()
        }
    getTableResults()
-
-   
-    //getTableResults()
-   /* function getTableResults(){
-   let tableHTML=`<table>`
-    let primarySelectedCity=$('#urbanAreas-Dropdown option:selected').text()
-    let secondarySelectedCity = $('#urbanAreas-Dropdown-secondary option:selected').text();
-    let tableHeadCityName =
-        `<tr class="header-wrapper">
-        <th class="categories">Categories</th>
-        <th class="primaryCity">${primarySelectedCity}</th>
-        <th class="secondaryCity">${secondarySelectedCity}</th>
-        <th class="percentDifference">Percent Difference</th>
-        <tr>`
-    
-   tableHTML += tableHeadCityName
-    //
-
-//calculates the percent diff of cities
-const scoreArr = STORE.primaryData.categories
-    
-    for (i = 0; i < scoreArr.length; i++) {
-        let percentDiff = ''
-        let secondaryValue = ''
-        if (STORE.secondaryData) {
-            const pValue = scoreArr[i].score_out_of_10
-            const secValue = STORE.secondaryData.categories[i].score_out_of_10
-            if (secValue !== 0) {
-                secondaryValue = secValue.toFixed(2)
-                percentDiff = 100 - pValue / secValue * 100
-                percentDiff = `${percentDiff.toFixed(2)}%`
-                console.log(scoreArr[i].name, pValue, secValue, percentDiff)
-            
-            }
-            
-        }
-//puts the scores in the DOM
-        const categoryNameScore = `
-        <tr class="scoreRowWrapper">
-            
-            <td class="scoreName">${scoreArr[i].name}</td>
-            <td class="scoreValue">
-            ${scoreArr[i].score_out_of_10.toFixed(2)}            
-            </td>
-            <td class="diffValue">
-            ${secondaryValue}            
-            </td>
-            <td class="diffValue">
-            ${percentDiff}            
-            </td>
-            
-        </tr>`
-
-        tableHTML +=categoryNameScore
-
-    }
-    tableHTML += '</table>'
-    $('#results-one').append(tableHTML)
-}*/
     if (!STORE.secondaryData) {
         $('.diffValue').hide()
         $('.primaryCity').hide()
         $('.secondaryCity').hide()
    $('.percentDifference').hide()
     }
-//*/
 }
 function watchForm() {
     urbanAreasDropdown();
