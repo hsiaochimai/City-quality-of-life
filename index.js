b2ca841fa640af16e13323f9e882495608a8c8a3
Jennifer Hsiao <jchmai21@gmail.com>
2019-03-03 16:10:08 -0800

    Added secondary sum and score to results

--- index.js
+++ index.js
@@ -116,12 +116,70 @@ function displayPrimaryCityResults(){
     $('#compareCity').show();
     $('#results-one').show();
     $('#results-one').removeClass('hidden')
-    let citySummary = normalizeSummary(STORE.primaryData.summary);
-    $('#results-one').append(`<p>${citySummary}</p>`);
-    let overallCityScore = `<p>Overall Score: ${STORE.primaryData.teleport_city_score.toFixed(2)}</p>`
-    $('#results-one').append(overallCityScore);
+    let primaryCitySummary = normalizeSummary(STORE.primaryData.summary);
+    $('#results-one').append(`<p class="primaryCitySumScore">${primaryCitySummary}</p>`);
+    let primaryOverallCityScore = `<p class="primaryCitySumScore">Overall Score: ${STORE.primaryData.teleport_city_score.toFixed(2)}</p>`
+    $('#results-one').append(primaryOverallCityScore);
     
 }
+function displaySecondaryCitySummary(){
+    let secondarycitySummary = normalizeSummary(STORE.secondaryData.summary);
+            $('#results-one').append(`<p class="secondaryCitySumScore">${secondarycitySummary}</p>`);
+            let secondaryoverallCityScore = `<p class="secondaryCitySumScore">Overall Score: ${STORE.secondaryData.teleport_city_score.toFixed(2)}</p>`
+            $('#results-one').append(secondaryoverallCityScore);
+}
+function getTableResults(){
+    let tableHTML=`<table>`
+     let primarySelectedCity=$('#urbanAreas-Dropdown option:selected').text()
+     let secondarySelectedCity = $('#urbanAreas-Dropdown-secondary option:selected').text();
+     let tableHeadCityName =
+         `<tr class="header-wrapper">
+         <th class="categories">Categories</th>
+         <th class="primaryCity">${primarySelectedCity}</th>
+         <th class="secondaryCity">${secondarySelectedCity}</th>
+         <th class="percentDifference">Percent Difference</th>
+         <tr>`
+    tableHTML += tableHeadCityName
+     //
+ 
+ //calculates the percent diff of cities
+ const scoreArr = STORE.primaryData.categories
+     
+     for (i = 0; i < scoreArr.length; i++) {
+         let percentDiff = ''
+         let secondaryValue = ''
+         if (STORE.secondaryData) {
+             const pValue = scoreArr[i].score_out_of_10
+             const secValue = STORE.secondaryData.categories[i].score_out_of_10
+             if (secValue !== 0) {
+                 secondaryValue = secValue.toFixed(2)
+                 percentDiff = 100 - pValue / secValue * 100
+                 percentDiff = `${percentDiff.toFixed(2)}%`
+                 console.log(scoreArr[i].name, pValue, secValue, percentDiff)
+             }  
+         }
+ //puts the scores in the DOM
+         const categoryNameScore = `
+         <tr class="scoreRowWrapper">
+             <td class="scoreName">${scoreArr[i].name}</td>
+             <td class="scoreValue">
+             ${scoreArr[i].score_out_of_10.toFixed(2)}            
+             </td>
+             <td class="diffValue">
+             ${secondaryValue}            
+             </td>
+             <td class="diffValue">
+             ${percentDiff}            
+             </td>
+             
+         </tr>`
+ 
+         tableHTML +=categoryNameScore
+ 
+     }
+     tableHTML += '</table>'
+     $('#results-one').append(tableHTML)
+ }
 
 function updateDOM() {
     $('#results-one').empty();
@@ -130,9 +188,15 @@ function updateDOM() {
         $('#results-one').hide();
         return
     }
-    
     displayPrimaryCityResults()
+    if (STORE.secondaryData){
+        displaySecondaryCitySummary()
+       }
+   getTableResults()
+
+   
     //getTableResults()
+   /* function getTableResults(){
    let tableHTML=`<table>`
     let primarySelectedCity=$('#urbanAreas-Dropdown option:selected').text()
     let secondarySelectedCity = $('#urbanAreas-Dropdown-secondary option:selected').text();
@@ -161,7 +225,9 @@ const scoreArr = STORE.primaryData.categories
                 percentDiff = 100 - pValue / secValue * 100
                 percentDiff = `${percentDiff.toFixed(2)}%`
                 console.log(scoreArr[i].name, pValue, secValue, percentDiff)
+            
             }
+            
         }
 //puts the scores in the DOM
         const categoryNameScore = `
@@ -185,7 +251,7 @@ const scoreArr = STORE.primaryData.categories
     }
     tableHTML += '</table>'
     $('#results-one').append(tableHTML)
-
+}*/
     if (!STORE.secondaryData) {
         $('.diffValue').hide()
         $('.primaryCity').hide()
